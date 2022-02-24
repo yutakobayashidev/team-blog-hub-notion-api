f1f5404001862548b9674fb7fd6279731c317edc
yutakobayashidev <yutakobayashi0924@gmail.com>
2022-02-24 18:28:26 +0900

    update: site Info

--- site.config.ts
+++ site.config.ts
@@ -1,25 +1,26 @@
 export const config = {
   siteMeta: {
-    title: "Team Blog Hub",
-    teamName: "catnose Inc.",
-    description: "RSS based blog starter kit for teams.",
+    title: 'Team Blog Hub Notion API',
+    teamName: 'Notion Labs, Inc.',
+    description: 'RSS based blog starter kit for teams. Powered by Notion API',
   },
   siteRoot:
-    process.env.NODE_ENV === "production"
-      ? "https://team-blog-hub.vercel.app"
-      : "http://localhost:3000",
+    process.env.NODE_ENV === 'production'
+      ? 'https://team-blog-hub.vercel.app'
+      : 'http://localhost:3000',
   headerLinks: [
     {
-      title: "About",
-      href: "/about",
+      title: 'About',
+      href: '/about',
     },
     {
-      title: "Company",
-      href: "https://zenn.dev/catnose99/articles/cb72a73368a547756862",
+      title: 'Notion',
+      href:
+        'https://yutakobayashi.notion.site/47bcd8cd498a4d3880a2ce793625b29d?v=bb2dc06f629c464e81e74692087d250f',
     },
     {
-      title: "GitHub",
-      href: "https://github.com/catnose99/team-blog-hub",
+      title: 'GitHub',
+      href: 'https://github.com/yutakobayashidev/team-blog-hub-notion-api',
     },
   ],
 };
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,35 +1,35 @@
-import { NextPage } from "next";
-import { ContentWrapper } from "@src/components/ContentWrapper";
-import { LinkBackHome } from "@src/components/LinkBackHome";
-import { PageSEO } from "@src/components/PageSEO";
+import { NextPage } from 'next';
+import { ContentWrapper } from '@src/components/ContentWrapper';
+import { LinkBackHome } from '@src/components/LinkBackHome';
+import { PageSEO } from '@src/components/PageSEO';
 
 const Page: NextPage = () => {
   return (
     <>
-      <PageSEO title="About" path="/about" />
+      <PageSEO title='About' path='/about' />
       <ContentWrapper>
-        <section className="about">
-          <h1 className="about__title">About</h1>
-          <div className="about__body">
+        <section className='about'>
+          <h1 className='about__title'>About</h1>
+          <div className='about__body'>
             <p>
               このサイトはチームのためのブログスターター
-              <a href="https://github.com/catnose99/team-blog-hub">
-                Team Blog Hub
+              <a href='https://github.com/yutakobayashidev/team-blog-hub-notion-api'>
+                Team Blog Hub Notion API
               </a>
-              のデモです。ブログのRSSのURLを登録することで、チームメンバーの投稿を一覧にまとめて表示します。
+              のデモです。ブログのRSSのURLをNotion上で登録することで、チームメンバーの投稿を一覧にまとめて表示します。
             </p>
             <p>
               Medium、note、Zenn、Qiita、はてなブログなど、RSSフィードを取得できるサイトであれば、メンバーは好きな場所に投稿できます。
             </p>
             <p>
-              詳しくは
-              <a href="https://zenn.dev/catnose99/articles/cb72a73368a547756862">
+              catnoseさんのリポジトリをフォークさせて頂きました。詳しくは
+              <a href='https://zenn.dev/catnose99/articles/cb72a73368a547756862'>
                 チーム個々人のテックブログをRSSで集約するサイトをNext.jsで作った
               </a>
               をご覧ください。
             </p>
           </div>
-          <div className="about__actions">
+          <div className='about__actions'>
             <LinkBackHome />
           </div>
         </section>
